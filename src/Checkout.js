1f3d2ee78f4995ccdd0485496dc1fad6f59d86c1
Rootul Patel <rootulp@palantir.com>
2017-10-07 17:17:28 +0100

    Input validation & set default state

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+
 class Checkout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      cardNumber: "",
+      csc: "",
+      expirationMonth: "",
+      expirationYear: ""
+    };
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
@@ -13,57 +20,109 @@ class Checkout extends React.Component {
     });
   }
 
+  renderErrors() {
+    if (!validCardNumber(this.state.cardNumber, this.state.csc)) {
+      return "Invalid Card Number"
+    }
+    if (!validCardExpiration(this.state.expirationMonth, this.state.expirationYear)) {
+      return "Invalid Expiration Date"
+    }
+  }
+
   render() {
     return (
-      <form>
-        <label>
-          Name
-          <input
-            name="name"
-            type="input"
-            checked={this.state.name}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Card Number
-          <input
-            name="cardNumber"
-            type="number"
-            value={this.state.cardNumber}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          CVV2
-          <input
-            name="cvv2"
-            type="number"
-            value={this.state.cvv2}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Expiration Month 
-          <input
-            name="expirationMonth"
-            type="number"
-            value={this.state.expirationMonth}
-            onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Expiration Year
-          <input
-            name="expirationYear"
-            type="number"
-            value={this.state.expirationYear}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="Checkout">
+        <span>{this.renderErrors()}</span>
+        <form>
+          <label>
+            Name
+            <input
+              name="name"
+              type="input"
+              checked={this.state.name}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Card Number
+            <input
+              name="cardNumber"
+              type="input"
+              value={this.state.cardNumber}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            csc
+            <input
+              name="csc"
+              type="input"
+              value={this.state.csc}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Expiration Month 
+            <input
+              name="expirationMonth"
+              type="input"
+              value={this.state.expirationMonth}
+              onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Expiration Year
+            <input
+              name="expirationYear"
+              type="input"
+              value={this.state.expirationYear}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
+}
+
+const validCardNumber = (cardNumber, csc) => {
+  return validVisa(cardNumber, csc) || validAmericanExpress(cardNumber, csc)
+}
+
+const validVisa = (cardNumber, csc) => {
+  return validVisaCardNumber(cardNumber) && validVisaCsc(csc);
+}
+
+const validAmericanExpress = (cardNumber, csc) => {
+  return validAmericanExpressCardNumber(cardNumber) && validAmericanExpressCsc(csc);
+}
+
+const validVisaCardNumber = (cardNumber) => {
+  if (cardNumber) {
+    return cardNumber.length === 16 && cardNumber.startsWith("4");
+  }
+  return false
+}
+
+const validAmericanExpressCardNumber = (cardNumber) => {
+  if (cardNumber) {
+    return cardNumber.length === 15 && 
+          (cardNumber.startsWith("34") ||
+            cardNumber.startsWith("37"));
+  }
+  return false
+}
+
+const validVisaCsc = (csc) => {
+  return csc ? csc.length === 4 : false
+}
+
+const validAmericanExpressCsc = (csc) => {
+  return csc ? csc.length === 3 : false
+}
+
+const validCardExpiration = (expirationMonth, expirationYear) => {
+  return true
 }
 
 export default Checkout;
