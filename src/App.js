2065d9dd8c91a429b865f48118952d6b5ba55ab8
Rootul Patel <rootulp@palantir.com>
2017-10-07 16:10:02 +0100

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import Checkout from './Checkout';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Checkout</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Checkout />
       </div>
     );
   }
