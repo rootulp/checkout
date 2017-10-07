f05807b739c1dc15879cf8d6d36c517ee32ccd6e
Rootul Patel <rootulp@palantir.com>
2017-10-07 19:17:56 +0100

    Prettier imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import 'normalize.css/normalize.css';
-import '@blueprintjs/core/dist/blueprint.css';
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/dist/blueprint.css";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
